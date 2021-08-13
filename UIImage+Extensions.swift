d3ab231b477993b242c6123ab16d23f2de45aee7
davalcato <davalcato@yahoo.com>
2021-08-13 15:55:04 -0700

    first commit

--- UIImage+Extensions.swift
+++ UIImage+Extensions.swift
@@ -12,18 +12,35 @@ extension UIImage {
     
     func resizeTo(size :CGSize) -> UIImage {
         
-        UIGraphicsBeginImageContext(size, false, 0.0)
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
         self.draw(in: CGRect(origin: CGPoint.zero, size: size))
         let resizedImage = UIGraphicsGetImageFromCurrentImageContext()!
         UIGraphicsEndImageContext()
         return resizedImage
-        
     }
     
     func toBuffer() -> CVPixelBuffer? {
         
         let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
-        var pixelbuff
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(self.size.width), Int(self.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else {
+            
+            return nil
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(
+            data: pixelData,
+            width: Int(self.size.width),
+            height: Int(self.size.height),
+            bitsPerComponent: 8,
+            bytesPerRow: <#T##Int#>,
+            space: <#T##CGColorSpace#>,
+            bitmapInfo: <#T##UInt32#>)
         
         
     }
